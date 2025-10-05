702dd49e89ea66e48217d122185caa08a936d9dd
suryansh-7777 <suryanshraghu10@gmail.com>
2025-10-05 13:07:46 +0530

    Create DAY25_B_DP.c

---
+++ DAY25_B_DP.c
@@ -0,0 +1,22 @@
+// Write a program to print the following pattern:
+//*****
+//****
+//***
+//**
+//*
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
