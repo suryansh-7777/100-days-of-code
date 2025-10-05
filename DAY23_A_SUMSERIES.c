24038df2d85b7133559e8ff35482dd520bb47971
suryansh-7777 <suryanshraghu10@gmail.com>
2025-10-05 13:00:38 +0530

    Create DAY23_A_SUMSERIES.c

---
+++ DAY23_A_SUMSERIES.c
@@ -0,0 +1,19 @@
+// Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    float sum = 0;
+
+    printf("Enter number of terms here: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum += (float)(2 * i) / (4 * i - 1);
+    }
+    printf("Approx sum: %.1f\n", sum);
+    return 0;
+}
