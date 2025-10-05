6828c1191ea3686d6f9b4d7a136d441f81471255
suryansh-7777 <suryanshraghu10@gmail.com>
2025-10-05 12:48:06 +0530

    Create DAY18_B_HCF.c

---
+++ DAY18_B_HCF.c
@@ -0,0 +1,21 @@
+//Q36: Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b; 
+        a = temp;  
+    }
+
+    printf("HCF = %d\n", a);
+
+    return 0;
+}
