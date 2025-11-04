e922bcb5ec1cdaea1b509d7979681ec76523eb32
suryansh-7777 <suryanshraghu10@gmail.com>
2025-11-04 08:50:43 +0530

    Create DAY45_A_FREQUENCY.c

---
+++ DAY45_A_FREQUENCY.c
@@ -0,0 +1,21 @@
+//Count frequency of a given character in a string.
+#include <stdio.h>
+
+int main() {
+    char str[200], ch;
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter a character to find frequency: ");
+    scanf("%c", &ch);
+
+    for(int i=0; str[i]!='\0'; i++) {
+        if(str[i]==ch)
+            count++;
+    }
+
+    printf("Frequency of '%c' = %d", ch, count);
+    return 0;
+}
