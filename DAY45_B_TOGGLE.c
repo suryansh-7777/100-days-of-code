73a1aa733cc2dea8d718d642416cdeef388deaff
suryansh-7777 <suryanshraghu10@gmail.com>
2025-11-04 08:51:46 +0530

    Create DAY45_B_TOGGLE.c

---
+++ DAY45_B_TOGGLE.c
@@ -0,0 +1,19 @@
+// Toggle case of each character in a string.
+#include <stdio.h>
+
+int main() {
+    char str[200];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(int i=0; str[i]!='\0'; i++) {
+        if(str[i]>='a' && str[i]<='z')
+            str[i] = str[i] - 32;
+        else if(str[i]>='A' && str[i]<='Z')
+            str[i] = str[i] + 32;
+    }
+
+    printf("Toggled string: %s", str);
+    return 0;
+}
