194fdc7e28782ea2c12e435e13c9d56f18ba8e2b
suryansh-7777 <suryanshraghu10@gmail.com>
2025-11-04 08:46:50 +0530

    Create Day43_B_palindromestring.c

---
+++ Day43_B_palindromestring.c
@@ -0,0 +1,22 @@
+//Check if a string is a palindrome.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, len = 0, flag = 1;
+    printf("Enter a string: ");
+    gets(str);
+    while(str[len] != '\0')
+        len++;
+    for(i = 0; i < len / 2; i++) {
+        if(str[i] != str[len - i - 1]) {
+            flag = 0;
+            break;
+        }
+    }
+    if(flag)
+        printf("String is a palindrome\n");
+    else
+        printf("String is not a palindrome\n");
+    return 0;
+}
