ac5726eb2cd9d38b9a2f534d622d27634730a29c
suryansh-7777 <suryanshraghu10@gmail.com>
2025-11-26 19:35:56 +0530

    Create DAY_74_A_FILE_COPYING.c

---
+++ DAY_74_A_FILE_COPYING.c
@@ -0,0 +1,40 @@
+/*Q124: Take two filenames from the user – a source file and a destination file. Copy all the content from the source file to the destination file using fgetc() and fputc().
+
+/*
+Sample Test Cases:
+Input 1:
+Source File: source.txt (Content: Learning C File Handling)
+Output 1:
+File copied successfully to destination.txt
+
+*/
+#include <stdio.h>
+int main() {
+    char sourceFile[100], destFile[100];
+    char ch;
+    printf("Enter source file name: ");
+    scanf("%s", sourceFile);
+    printf("Enter destination file name: ");
+    scanf("%s", destFile);
+    FILE *source = fopen(sourceFile, "r");
+    if (source == NULL) {
+        printf("Error opening source file.\n");
+        return 1;
+    }
+
+
+    FILE *dest = fopen(destFile, "w");
+    if (dest == NULL) {
+        printf("Error opening destination file.\n");
+        fclose(source);
+        return 1;
+    }
+    while ((ch = fgetc(source)) != EOF) {
+        fputc(ch, dest);
+    }
+    fclose(source);
+    fclose(dest);
+
+    printf("File copied successfully to %s\n", destFile);
+    return 0;
+}
